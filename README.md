de594d9863edc007f23b9c4c679a2fc0a44a9f71
unknown <saianjan1234@gmail.com>
2023-11-15 20:44:19 +0530

    Added Subtraction feature!

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ Calculator App with Basic Funtionalities!
 The Appliction takes input of two numbers from the user.
 And then computes the addition of the two numbers.
 Displays the result sum of the numbers.
+*Added Subtraction feature to the Calculator App.
+Computes the subtraction of the numbers and displays the result.
--- calc.sh
+++ calc.sh
@@ -3,5 +3,20 @@ echo "Enter the 1st number:"
 read a
 echo "Enter the 2nd number:"
 read b
-sum=$(($a+$b))
-echo "sum of $a+$b =" $sum
+echo "Enter the choice:"
+echo "1: Addition"
+echo "2: Subtraction"
+read input
+
+case $input in 
+	1)
+		sum=$(($a+$b))
+		echo "sum of $a+$b =" $sum
+		;;
+	2)
+		sub=$(($a-$b))
+		echo "Subtraction of $a-$b =" $sub
+		;;
+	*)
+		echo "Invaild Input!"
+esac
